e75c1fce7b21e5d45575402b27ae03b995305a09
mxmmsv <oikioikioikiabrams@gmail.com>
2024-11-06 16:44:48 +0300

    feat: add removeFirstOccurrences task

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -152,8 +152,8 @@ function repeatString(str, times) {
  *   removeFirstOccurrences('I like legends', 'end') => 'I like legs'.
  *   removeFirstOccurrences('ABABAB', 'BA') => 'ABAB'.
  */
-function removeFirstOccurrences(/* str, value */) {
-  throw new Error('Not implemented');
+function removeFirstOccurrences(str, value) {
+  return str.indexOf(value) === -1 ? str : str.replace(value, '');
 }
 
 /**
