9c3c32b8401bc9c47d3ad31f739e2684e5f84b77
mxmmsv <oikioikioikiabrams@gmail.com>
2024-11-08 02:20:23 +0300

    feat: add reverseWords task

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -386,8 +386,15 @@ function findLongestWord(sentence) {
  *   reverseWords('Hello World') => 'olleH dlroW'
  *   reverseWords('The Quick Brown Fox') => 'ehT kciuQ nworB xoF'
  */
-function reverseWords(/* str */) {
-  throw new Error('Not implemented');
+function reverseWords(str) {
+  const strToArray = str.split(' ');
+  let reverseWord;
+  const finalFinal = [];
+  for (let i = 0; i < strToArray.length; i += 1) {
+    reverseWord = strToArray[i].split('').reverse().join('');
+    finalFinal.push(reverseWord);
+  }
+  return finalFinal.join(' ');
 }
 
 /**
